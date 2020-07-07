23f4a4d6ae892c5232c143a9896e88d08f3f03d5
Nick Escalona <nicholas.escalona@gmail.com>
2020-07-07 14:07:43 -0500

    fix in different way

---
+++ Project0.test/LocationControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.test
+{
+    public class LocationControllerTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
